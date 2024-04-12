383976eedcefbfc6dfd0c6e7e516ee6a99fbd108
ericseferian <seferian123@gmail.com>
2024-04-12 14:23:42 +0400

    Created Dealer Class

---
+++ Dealer.java
@@ -0,0 +1,11 @@
+//	•	Deals 12 cards for gameplay
+//	•	If no set is present, add 3 additional cards into the game
+//	•	Detect when there are no cards left
+// removes the used cards from the deck
+//potentially interacts with a board object
+
+public class Dealer {
+
+
+
+}
